bad6be30f946ba57d344b5419011cb405fee98f9
Bishal Sunuwar <bishalsunuwar24@gmail.com>
2023-09-17 09:07:29 +0545

    another commit

--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,16 @@ const express = require("express");
 const router = express.Router();
 const rootDir = require("../util/path");
 
+const adminData = require("./admin");
+
 router.get("/", (req, res, next) => {
+  console.log("shopJs:", adminData.products);
   //res.send("<p>First One</p>");
   //res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  //res.sendFile(path.join(rootDir, "views", "shop.html"));
 
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: 'Shop' });
 });
 
 module.exports = router;
